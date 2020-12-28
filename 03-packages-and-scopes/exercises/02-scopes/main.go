35b5c872cb958763bf9dd886b9722ae2e35479cd
jeremy.d-auria <jeremy.d-auria@epita.fr>
2020-12-28 23:58:23 +0100

    03: 02-scopes

--- 03-packages-and-scopes/exercises/02-scopes/main.go
+++ 03-packages-and-scopes/exercises/02-scopes/main.go
@@ -28,5 +28,12 @@ package main
 //        inside the hello function
 // ---------------------------------------------------------
 
+import "fmt"
+
 func main() {
+	hello()
+}
+
+func bye() {
+	fmt.Println("bye bye")
 }
